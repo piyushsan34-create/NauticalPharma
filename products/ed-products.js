544cbcc9076bce09167199a3798c0e74e99abc4a
piyushsan34-create <piyushsan34@gmail.com>
2026-07-16 17:35:38 +0530

    Update ed-products.js

--- products/ed-products.js
+++ products/ed-products.js
@@ -1,39 +1,131 @@
 const products = [
 
 {
-name: "Cenforce 100",
-generic: "Sildenafil Citrate",
-strength: "100 mg",
-packing: "10 x 10 Tablets",
-manufacturer: "Centurion Laboratories",
-image: "images/cenforce100.jpg"
+name:"Cenforce 25",
+generic:"Sildenafil Citrate",
+strength:"25 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
 },
 
 {
-name: "Kamagra Gold 100",
-generic: "Sildenafil Citrate",
-strength: "100 mg",
-packing: "10 x 10 Tablets",
-manufacturer: "Ajanta Pharma",
-image: "images/kamagra.jpg"
+name:"Cenforce 50",
+generic:"Sildenafil Citrate",
+strength:"50 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
 },
 
 {
-name: "Vidalista 20",
-generic: "Tadalafil",
-strength: "20 mg",
-packing: "10 x 10 Tablets",
-manufacturer: "Centurion Laboratories",
-image: "images/vidalista.jpg"
+name:"Cenforce 100",
+generic:"Sildenafil Citrate",
+strength:"100 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
 },
 
 {
-name: "Tadacip 20",
-generic: "Tadalafil",
-strength: "20 mg",
-packing: "10 x 10 Tablets",
-manufacturer: "Cipla",
-image: "images/tadacip.jpg"
+name:"Cenforce 120",
+generic:"Sildenafil Citrate",
+strength:"120 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+
+{
+name:"Cenforce 150",
+generic:"Sildenafil Citrate",
+strength:"150 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+
+{
+name:"Cenforce 200",
+generic:"Sildenafil Citrate",
+strength:"200 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+
+{
+name:"Kamagra Gold 100",
+generic:"Sildenafil Citrate",
+strength:"100 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Ajanta Pharma"
+},
+
+{
+name:"Suhagra 100",
+generic:"Sildenafil Citrate",
+strength:"100 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Cipla"
+},
+
+{
+name:"Malegra 100",
+generic:"Sildenafil Citrate",
+strength:"100 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Sunrise Remedies"
+},
+
+{
+name:"Fildena 100",
+generic:"Sildenafil Citrate",
+strength:"100 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Fortune Healthcare"
+},
+
+{
+name:"Vidalista 20",
+generic:"Tadalafil",
+strength:"20 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+
+{
+name:"Vidalista 40",
+generic:"Tadalafil",
+strength:"40 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+
+{
+name:"Vidalista 60",
+generic:"Tadalafil",
+strength:"60 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+
+{
+name:"Tadalista 20",
+generic:"Tadalafil",
+strength:"20 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Fortune Healthcare"
+},
+
+{
+name:"Vilitra 20",
+generic:"Vardenafil",
+strength:"20 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+
+{
+name:"Avana 100",
+generic:"Avanafil",
+strength:"100 mg",
+packing:"4 x 4 Tablets",
+manufacturer:"Sunrise Remedies"
 }
 
 ];
