63d1a4f1fd137f59bae696715c2e500df5d8720e
piyushsan34-create <piyushsan34@gmail.com>
2026-07-16 19:15:17 +0530

    Refactor product display and filtering functionality

--- products/ed-products.js
+++ products/ed-products.js
@@ -1,35 +1,112 @@
+const products = [
+{
+name:"Cenforce 25",
+generic:"Sildenafil Citrate",
+strength:"25 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+{
+name:"Cenforce 50",
+generic:"Sildenafil Citrate",
+strength:"50 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+{
+name:"Cenforce 100",
+generic:"Sildenafil Citrate",
+strength:"100 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+{
+name:"Kamagra Gold 100",
+generic:"Sildenafil Citrate",
+strength:"100 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Ajanta Pharma"
+},
+{
+name:"Vidalista 20",
+generic:"Tadalafil",
+strength:"20 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+{
+name:"Vilitra 20",
+generic:"Vardenafil",
+strength:"20 mg",
+packing:"10 x 10 Tablets",
+manufacturer:"Centurion Laboratories"
+},
+{
+name:"Avana 100",
+generic:"Avanafil",
+strength:"100 mg",
+packing:"4 x 4 Tablets",
+manufacturer:"Sunrise Remedies"
+}
+];
+
+const grid = document.getElementById("productGrid");
+const search = document.getElementById("searchInput");
+const filter = document.getElementById("categoryFilter");
+
 function displayProducts(list){
 
-grid.innerHTML="";
+    grid.innerHTML = "";
 
-list.forEach(product=>{
+    list.forEach(product => {
 
-grid.innerHTML+=`
+        grid.innerHTML += `
+        <div class="product-card">
 
-<div class="product-card">
+            <h3>${product.name}</h3>
 
-<img src="../images/no-image.png" alt="${product.name}">
+            <p><strong>Generic:</strong> ${product.generic}</p>
 
-<h3>${product.name}</h3>
+            <p><strong>Strength:</strong> ${product.strength}</p>
 
-<p><strong>Generic:</strong> ${product.generic}</p>
+            <p><strong>Packing:</strong> ${product.packing}</p>
 
-<p><strong>Strength:</strong> ${product.strength}</p>
+            <p><strong>Manufacturer:</strong> ${product.manufacturer}</p>
 
-<p><strong>Packing:</strong> ${product.packing}</p>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdfMsIqbKJQlSclhLTOwSiU7SaShe-y_R9y6r875sU-gO9jkg/viewform?usp=header"
+               class="quote-btn"
+               target="_blank">
 
-<p><strong>Manufacturer:</strong> ${product.manufacturer}</p>
+               Request Quote
 
-<a href="https://docs.google.com/forms/d/e/1FAIpQLSdfMsIqbKJQlSclhLTOwSiU7SaShe-y_R9y6r875sU-gO9jkg/viewform?usp=header" class="quote-btn" target="_blank">
+            </a>
 
-Request Quote
+        </div>
+        `;
 
-</a>
+    });
 
-</div>
+}
 
-`;
+displayProducts(products);
 
-});
+search.addEventListener("keyup", filterProducts);
+filter.addEventListener("change", filterProducts);
+
+function filterProducts(){
+
+    const keyword = search.value.toLowerCase();
+    const generic = filter.value.toLowerCase();
+
+    const filtered = products.filter(product => {
+
+        return (
+            product.name.toLowerCase().includes(keyword) &&
+            (generic === "" || product.generic.toLowerCase().includes(generic))
+        );
+
+    });
+
+    displayProducts(filtered);
 
 }
