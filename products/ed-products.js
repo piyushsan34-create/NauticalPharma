8b06f8d727bcc30359a107141f81dbb7dde6801a
piyushsan34-create <piyushsan34@gmail.com>
2026-07-16 16:43:26 +0530

    Add product details to ed-products.js
    Added multiple products with details including name, generic, strength, packing, manufacturer, and image.

--- products/ed-products.js
+++ products/ed-products.js
@@ -1,1 +1,39 @@
+const products = [
 
+{
+name: "Cenforce 100",
+generic: "Sildenafil Citrate",
+strength: "100 mg",
+packing: "10 x 10 Tablets",
+manufacturer: "Centurion Laboratories",
+image: "images/cenforce100.jpg"
+},
+
+{
+name: "Kamagra Gold 100",
+generic: "Sildenafil Citrate",
+strength: "100 mg",
+packing: "10 x 10 Tablets",
+manufacturer: "Ajanta Pharma",
+image: "images/kamagra.jpg"
+},
+
+{
+name: "Vidalista 20",
+generic: "Tadalafil",
+strength: "20 mg",
+packing: "10 x 10 Tablets",
+manufacturer: "Centurion Laboratories",
+image: "images/vidalista.jpg"
+},
+
+{
+name: "Tadacip 20",
+generic: "Tadalafil",
+strength: "20 mg",
+packing: "10 x 10 Tablets",
+manufacturer: "Cipla",
+image: "images/tadacip.jpg"
+}
+
+];
