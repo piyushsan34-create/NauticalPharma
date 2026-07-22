2c7bec9268f20dd484fe6c8b1024a5bb20cf032a
piyushsan34-create <piyushsan34@gmail.com>
2026-07-22 18:03:14 +0530

    Update product pages with query parameters

--- products/all-products.js
+++ products/all-products.js
@@ -4,21 +4,21 @@ const allProducts = [
     category: "ED Medicines",
     name: "Cenforce 100",
     generic: "Sildenafil Citrate",
-    page: "products/ed-medicines.html"
+    page: "products/ed-medicines.html?product=Cenforce%20100"
 },
 
 {
     category: "ED Medicines",
     name: "Vidalista 20",
     generic: "Tadalafil",
-    page: "products/ed-medicines.html"
+    page: "products/ed-medicines.html?product=Vidalista%2020"
 },
 
 {
     category: "ED Medicines",
     name: "Kamagra Gold",
     generic: "Sildenafil Citrate",
-    page: "products/ed-medicines.html"
+    page: "products/ed-medicines.html?product=Kamagra%20Gold"
 }
 
 ];
