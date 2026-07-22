d28f1956693076770f1da704957f1d02d1d873e7
piyushsan34-create <piyushsan34@gmail.com>
2026-07-22 16:09:41 +0530

    Populate allProducts with ED medicine data
    Added a list of erectile dysfunction medicines with details.

--- products/all-products.js
+++ products/all-products.js
@@ -1,3 +1,26 @@
-const allProducts = [];
+const allProducts = [
+
+{
+    category: "ED Medicines",
+    name: "Cenforce 100",
+    generic: "Sildenafil Citrate",
+    page: "products/ed-medicines.html"
+},
+
+{
+    category: "ED Medicines",
+    name: "Vidalista 20",
+    generic: "Tadalafil",
+    page: "products/ed-medicines.html"
+},
+
+{
+    category: "ED Medicines",
+    name: "Kamagra Gold",
+    generic: "Sildenafil Citrate",
+    page: "products/ed-medicines.html"
+}
+
+];
 
 window.allProducts = allProducts;
