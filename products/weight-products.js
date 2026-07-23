1e18859db0a2c1187af99b27ce65382e86f49acf
piyushsan34-create <piyushsan34@gmail.com>
2026-07-23 18:20:01 +0530

    Update weight-products.js with new medications
    Removed multiple Sildenafil Citrate products and added Mounjaro and Wegovy products.

--- products/weight-products.js
+++ products/weight-products.js
@@ -1,123 +1,76 @@
 const products = [
+{
+    name: "Mounjaro",
+    generic: "Tirzepatide Injection",
+    strength: "2.5mg/0.5ml",
+    packing: "Single Dose Vial",
+    manufacturer: "Eli Lilly"
+},
 
-/* ===========================
-   Sildenafil Citrate
-=========================== */
+{
+    name: "Mounjaro",
+    generic: "Tirzepatide Injection",
+    strength: "5mg/0.5ml",
+    packing: "Single Dose Vial",
+    manufacturer: "Eli Lilly"
+},
 
-{name:"Cenforce 25",generic:"Sildenafil Citrate",strength:"25 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Cenforce 50",generic:"Sildenafil Citrate",strength:"50 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Cenforce 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Cenforce 120",generic:"Sildenafil Citrate",strength:"120 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Cenforce 130",generic:"Sildenafil Citrate",strength:"130 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Cenforce 150",generic:"Sildenafil Citrate",strength:"150 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Cenforce 200",generic:"Sildenafil Citrate",strength:"200 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Cenforce FM",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Cenforce Soft",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Cenforce Professional",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "2.5mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
 
-{name:"Kamagra Gold",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Ajanta Pharma"},
-{name:"Kamagra CT",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Ajanta Pharma"},
-{name:"Kamagra Polo",generic:"Sildenafil Citrate",strength:"100 mg",packing:"4 x 7 Tablets",manufacturer:"Ajanta Pharma"},
-{name:"Kamagra Oral Jelly",generic:"Sildenafil Citrate",strength:"100 mg",packing:"7 Sachets",manufacturer:"Ajanta Pharma"},
-{name:"Kamagra Export",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Ajanta Pharma"},
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "5mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
 
-{name:"Suhagra 25",generic:"Sildenafil Citrate",strength:"25 mg",packing:"10 x 10 Tablets",manufacturer:"Cipla"},
-{name:"Suhagra 50",generic:"Sildenafil Citrate",strength:"50 mg",packing:"10 x 10 Tablets",manufacturer:"Cipla"},
-{name:"Suhagra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Cipla"},
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "7.5mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
 
-{name:"Malegra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Malegra 200",generic:"Sildenafil Citrate",strength:"200 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "10mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
 
-{name:"Silditop 50",generic:"Sildenafil Citrate",strength:"50 mg",packing:"10 x 10 Tablets",manufacturer:"Healing Pharma"},
-{name:"Silditop 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Healing Pharma"},
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "12.5mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
 
-{name:"Aurogra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Aurochem"},
-{name:"Zenegra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Alkem Laboratories"},
-{name:"Penegra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Zydus Healthcare"},
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "15mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
 
-{name:"Fildena 50",generic:"Sildenafil Citrate",strength:"50 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
-{name:"Fildena 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
-{name:"Fildena Strong",generic:"Sildenafil Citrate",strength:"120 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+{
+    name: "Wegovy FlexTouch",
+    generic: "Semaglutide Injection",
+    strength: "0.25mg",
+    packing: "1.5ml Pre-filled Pen",
+    manufacturer: "Novo Nordisk"
+}
 
-{name:"Leeforce 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Leeford"},
-{name:"Vigore 50",generic:"Sildenafil Citrate",strength:"50 mg",packing:"10 x 10 Tablets",manufacturer:"German Remedies"},
-{name:"Vigore 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"German Remedies"},
-{name:"Lovegra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Ajanta Pharma"},
-{name:"Vega 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"HAB Pharma"},
-
-/* ===========================
-   Tadalafil
-=========================== */
-
-{name:"Vidalista 2.5",generic:"Tadalafil",strength:"2.5 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vidalista 5",generic:"Tadalafil",strength:"5 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vidalista 10",generic:"Tadalafil",strength:"10 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vidalista 20",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vidalista 40",generic:"Tadalafil",strength:"40 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vidalista 60",generic:"Tadalafil",strength:"60 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vidalista 80",generic:"Tadalafil",strength:"80 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vidalista Professional",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vidalista CT",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-
-{name:"Tadalista 5",generic:"Tadalafil",strength:"5 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
-{name:"Tadalista 10",generic:"Tadalafil",strength:"10 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
-{name:"Tadalista 20",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
-{name:"Tadalista 40",generic:"Tadalafil",strength:"40 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
-{name:"Tadalista 60",generic:"Tadalafil",strength:"60 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
-{name:"Tadalista Pro",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
-{name:"Tadalista Super Active",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Capsules",manufacturer:"Fortune Healthcare"},
-
-{name:"Tadalee 20",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Tadalee 40",generic:"Tadalafil",strength:"40 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Tadalee 60",generic:"Tadalafil",strength:"60 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
-
-{name:"Cialis",generic:"Tadalafil",strength:"20 mg",packing:"1 x 4 Tablets",manufacturer:"Lilly"},
-
-/* ===========================
-   Vardenafil
-=========================== */
-
-{name:"Vilitra 10",generic:"Vardenafil",strength:"10 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vilitra 20",generic:"Vardenafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vilitra 40",generic:"Vardenafil",strength:"40 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vilitra 60",generic:"Vardenafil",strength:"60 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Vitara V 20",generic:"Vardenafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Signature"},
-{name:"Filitra Professional",generic:"Vardenafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
-{name:"Zhewitra 20",generic:"Vardenafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Zhewitra 40",generic:"Vardenafil",strength:"40 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
-
-/* ===========================
-   Avanafil
-=========================== */
-
-{name:"Avana 50",generic:"Avanafil",strength:"50 mg",packing:"4 x 4 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Avana 100",generic:"Avanafil",strength:"100 mg",packing:"4 x 4 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Avana 200",generic:"Avanafil",strength:"200 mg",packing:"4 x 4 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Avaforce 100",generic:"Avanafil",strength:"100 mg",packing:"4 x 4 Tablets",manufacturer:"Centurion Laboratories"},
-
-/* ===========================
-   Udenafil
-=========================== */
-
-{name:"Zudena 100",generic:"Udenafil",strength:"100 mg",packing:"4 x 4 Tablets",manufacturer:"Sunrise Remedies"},
-   
-/* ===========================
-   ED Combination Products
-=========================== */
-
-{name:"Super P Force",generic:"Sildenafil + Dapoxetine",strength:"100 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Extra Super P Force",generic:"Sildenafil + Dapoxetine",strength:"200 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Super Kamagra",generic:"Sildenafil + Dapoxetine",strength:"100 mg + 60 mg",packing:"4 x 4 Tablets",manufacturer:"Ajanta Pharma"},
-{name:"Super Fildena",generic:"Sildenafil + Dapoxetine",strength:"100 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
-{name:"Super Cenforce",generic:"Sildenafil + Dapoxetine",strength:"100 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-
-{name:"Super Vidalista",generic:"Tadalafil + Dapoxetine",strength:"20 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-{name:"Super Tadarise",generic:"Tadalafil + Dapoxetine",strength:"20 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Tadapox",generic:"Tadalafil + Dapoxetine",strength:"20 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
-
-{name:"Sildalist",generic:"Sildenafil + Tadalafil",strength:"100 mg + 20 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Silvitra",generic:"Sildenafil + Vardenafil",strength:"100 mg + 20 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
-{name:"Tadacip",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Cipla"}
 ];
 window.weightProducts = products;
 const search = document.getElementById("searchInput");
