763cfae59bc718370d07707b485efda32172b8b2
piyushsan34-create <piyushsan34@gmail.com>
2026-07-23 17:34:40 +0530

    Add product details for Mounjaro and Wegovy

--- products/weight-products.js
+++ products/weight-products.js
@@ -1,1 +1,35 @@
+const products = [
 
+{
+    name: "Mounjaro",
+    generic: "Tirzepatide Injection",
+    strength: "2.5mg/0.5ml",
+    packing: "Single Dose Vial",
+    manufacturer: "Eli Lilly"
+},
+
+{
+    name: "Mounjaro",
+    generic: "Tirzepatide Injection",
+    strength: "5mg/0.5ml",
+    packing: "Single Dose Vial",
+    manufacturer: "Eli Lilly"
+},
+
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "2.5mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
+
+{
+    name: "Wegovy FlexTouch",
+    generic: "Semaglutide Injection",
+    strength: "0.25mg",
+    packing: "1.5ml Pre-filled Pen",
+    manufacturer: "Novo Nordisk"
+}
+
+];
