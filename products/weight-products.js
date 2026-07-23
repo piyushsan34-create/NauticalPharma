a1aceb2d7a8cfa2333dd19c3d117b34858c66b5b
piyushsan34-create <piyushsan34@gmail.com>
2026-07-23 18:08:31 +0530

    Add weight products data to weight-products.js

--- products/weight-products.js
+++ products/weight-products.js
@@ -1,1 +1,348 @@
+const products = [
 
+/* ===========================
+   Sildenafil Citrate
+=========================== */
+
+{name:"Cenforce 25",generic:"Sildenafil Citrate",strength:"25 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Cenforce 50",generic:"Sildenafil Citrate",strength:"50 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Cenforce 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Cenforce 120",generic:"Sildenafil Citrate",strength:"120 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Cenforce 130",generic:"Sildenafil Citrate",strength:"130 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Cenforce 150",generic:"Sildenafil Citrate",strength:"150 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Cenforce 200",generic:"Sildenafil Citrate",strength:"200 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Cenforce FM",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Cenforce Soft",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Cenforce Professional",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+
+{name:"Kamagra Gold",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Ajanta Pharma"},
+{name:"Kamagra CT",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Ajanta Pharma"},
+{name:"Kamagra Polo",generic:"Sildenafil Citrate",strength:"100 mg",packing:"4 x 7 Tablets",manufacturer:"Ajanta Pharma"},
+{name:"Kamagra Oral Jelly",generic:"Sildenafil Citrate",strength:"100 mg",packing:"7 Sachets",manufacturer:"Ajanta Pharma"},
+{name:"Kamagra Export",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Ajanta Pharma"},
+
+{name:"Suhagra 25",generic:"Sildenafil Citrate",strength:"25 mg",packing:"10 x 10 Tablets",manufacturer:"Cipla"},
+{name:"Suhagra 50",generic:"Sildenafil Citrate",strength:"50 mg",packing:"10 x 10 Tablets",manufacturer:"Cipla"},
+{name:"Suhagra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Cipla"},
+
+{name:"Malegra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Malegra 200",generic:"Sildenafil Citrate",strength:"200 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+
+{name:"Silditop 50",generic:"Sildenafil Citrate",strength:"50 mg",packing:"10 x 10 Tablets",manufacturer:"Healing Pharma"},
+{name:"Silditop 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Healing Pharma"},
+
+{name:"Aurogra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Aurochem"},
+{name:"Zenegra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Alkem Laboratories"},
+{name:"Penegra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Zydus Healthcare"},
+
+{name:"Fildena 50",generic:"Sildenafil Citrate",strength:"50 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+{name:"Fildena 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+{name:"Fildena Strong",generic:"Sildenafil Citrate",strength:"120 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+
+{name:"Leeforce 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Leeford"},
+{name:"Vigore 50",generic:"Sildenafil Citrate",strength:"50 mg",packing:"10 x 10 Tablets",manufacturer:"German Remedies"},
+{name:"Vigore 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"German Remedies"},
+{name:"Lovegra 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"Ajanta Pharma"},
+{name:"Vega 100",generic:"Sildenafil Citrate",strength:"100 mg",packing:"10 x 10 Tablets",manufacturer:"HAB Pharma"},
+
+/* ===========================
+   Tadalafil
+=========================== */
+
+{name:"Vidalista 2.5",generic:"Tadalafil",strength:"2.5 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vidalista 5",generic:"Tadalafil",strength:"5 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vidalista 10",generic:"Tadalafil",strength:"10 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vidalista 20",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vidalista 40",generic:"Tadalafil",strength:"40 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vidalista 60",generic:"Tadalafil",strength:"60 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vidalista 80",generic:"Tadalafil",strength:"80 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vidalista Professional",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vidalista CT",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+
+{name:"Tadalista 5",generic:"Tadalafil",strength:"5 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+{name:"Tadalista 10",generic:"Tadalafil",strength:"10 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+{name:"Tadalista 20",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+{name:"Tadalista 40",generic:"Tadalafil",strength:"40 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+{name:"Tadalista 60",generic:"Tadalafil",strength:"60 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+{name:"Tadalista Pro",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+{name:"Tadalista Super Active",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Capsules",manufacturer:"Fortune Healthcare"},
+
+{name:"Tadalee 20",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Tadalee 40",generic:"Tadalafil",strength:"40 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Tadalee 60",generic:"Tadalafil",strength:"60 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+
+{name:"Cialis",generic:"Tadalafil",strength:"20 mg",packing:"1 x 4 Tablets",manufacturer:"Lilly"},
+
+/* ===========================
+   Vardenafil
+=========================== */
+
+{name:"Vilitra 10",generic:"Vardenafil",strength:"10 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vilitra 20",generic:"Vardenafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vilitra 40",generic:"Vardenafil",strength:"40 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vilitra 60",generic:"Vardenafil",strength:"60 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Vitara V 20",generic:"Vardenafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Signature"},
+{name:"Filitra Professional",generic:"Vardenafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+{name:"Zhewitra 20",generic:"Vardenafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Zhewitra 40",generic:"Vardenafil",strength:"40 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+
+/* ===========================
+   Avanafil
+=========================== */
+
+{name:"Avana 50",generic:"Avanafil",strength:"50 mg",packing:"4 x 4 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Avana 100",generic:"Avanafil",strength:"100 mg",packing:"4 x 4 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Avana 200",generic:"Avanafil",strength:"200 mg",packing:"4 x 4 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Avaforce 100",generic:"Avanafil",strength:"100 mg",packing:"4 x 4 Tablets",manufacturer:"Centurion Laboratories"},
+
+/* ===========================
+   Udenafil
+=========================== */
+
+{name:"Zudena 100",generic:"Udenafil",strength:"100 mg",packing:"4 x 4 Tablets",manufacturer:"Sunrise Remedies"},
+   
+/* ===========================
+   ED Combination Products
+=========================== */
+
+{name:"Super P Force",generic:"Sildenafil + Dapoxetine",strength:"100 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Extra Super P Force",generic:"Sildenafil + Dapoxetine",strength:"200 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Super Kamagra",generic:"Sildenafil + Dapoxetine",strength:"100 mg + 60 mg",packing:"4 x 4 Tablets",manufacturer:"Ajanta Pharma"},
+{name:"Super Fildena",generic:"Sildenafil + Dapoxetine",strength:"100 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Fortune Healthcare"},
+{name:"Super Cenforce",generic:"Sildenafil + Dapoxetine",strength:"100 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+
+{name:"Super Vidalista",generic:"Tadalafil + Dapoxetine",strength:"20 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+{name:"Super Tadarise",generic:"Tadalafil + Dapoxetine",strength:"20 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Tadapox",generic:"Tadalafil + Dapoxetine",strength:"20 mg + 60 mg",packing:"10 x 10 Tablets",manufacturer:"Centurion Laboratories"},
+
+{name:"Sildalist",generic:"Sildenafil + Tadalafil",strength:"100 mg + 20 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Silvitra",generic:"Sildenafil + Vardenafil",strength:"100 mg + 20 mg",packing:"10 x 10 Tablets",manufacturer:"Sunrise Remedies"},
+{name:"Tadacip",generic:"Tadalafil",strength:"20 mg",packing:"10 x 10 Tablets",manufacturer:"Cipla"}
+];
+window.weightProducts = products;
+const search = document.getElementById("searchInput");
+const filter = document.getElementById("categoryFilter");
+const manufacturer = document.getElementById("manufacturerFilter");
+// Populate manufacturer dropdown automatically
+const manufacturers = [...new Set(products.map(product => product.manufacturer))].sort();
+
+manufacturers.forEach(company => {
+
+    const option = document.createElement("option");
+    option.value = company;
+    option.textContent = company;
+    manufacturer.appendChild(option);
+
+});
+const count = document.getElementById("productCount");
+
+function displayProducts(list){
+
+    const container = document.getElementById("productContainer");
+
+    container.innerHTML = "";
+
+    count.textContent = `${list.length} Products Found`;
+
+const groups = {};
+   
+    list.forEach(product=>{
+
+        if(!groups[product.generic]){
+            groups[product.generic]=[];
+        }
+
+        groups[product.generic].push(product);
+
+    });
+
+    for(const generic in groups){
+
+        container.innerHTML += `
+
+        <div class="accordion">
+
+            <div class="accordion-header">
+
+                <div class="accordion-title">
+                    ${generic}
+                    <span class="count">(${groups[generic].length} Products)</span>
+                </div>
+
+                <div class="accordion-icon">▼</div>
+
+            </div>
+
+            <div class="accordion-content">
+
+                <div class="product-grid">
+
+                    ${groups[generic].map(product=>`
+
+                    <div class="product-card"
+     id="${product.name.toLowerCase().replace(/\s+/g,'-')}"
+     onclick="openProduct(
+'${product.name}',
+'${product.generic}',
+'${product.strength}',
+'${product.packing}',
+'${product.manufacturer}'
+)">
+
+                        <h3>${product.name}</h3>
+
+                        <p><strong>Strength:</strong> ${product.strength}</p>
+
+                        <p><strong>Packing:</strong> ${product.packing}</p>
+
+                        <p><strong>Manufacturer:</strong> ${product.manufacturer}</p>
+
+                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdfMsIqbKJQlSclhLTOwSiU7SaShe-y_R9y6r875sU-gO9jkg/viewform?usp=header"
+                        class="quote-btn"
+                        target="_blank">
+                        Request Quote
+                        </a>
+
+                    </div>
+
+                    `).join("")}
+
+                </div>
+
+            </div>
+
+        </div>
+
+        `;
+
+    }
+
+    document.querySelectorAll(".accordion-header").forEach(header=>{
+
+        header.addEventListener("click",()=>{
+
+            header.parentElement.classList.toggle("active");
+
+        });
+
+    });
+} // <-- closes displayProducts()
+
+function filterProducts(){
+
+    const keyword = search.value.toLowerCase().trim();
+    const generic = filter.value.toLowerCase();
+    const company = manufacturer.value.toLowerCase();
+
+    const filtered = products.filter(product => {
+
+        const matchSearch =
+            product.name.toLowerCase().includes(keyword) ||
+            product.generic.toLowerCase().includes(keyword) ||
+            product.manufacturer.toLowerCase().includes(keyword);
+
+        const matchGeneric =
+            generic === "" ||
+            product.generic.toLowerCase().includes(generic);
+
+        const matchManufacturer =
+            company === "" ||
+            product.manufacturer.toLowerCase() === company;
+
+        return matchSearch && matchGeneric && matchManufacturer;
+
+    });
+
+    displayProducts(filtered);
+
+}
+// ===============================
+// PRODUCT POPUP
+// ===============================
+
+function openProduct(name,generic,strength,packing,manufacturer){
+
+    document.getElementById("modalName").textContent = name;
+    document.getElementById("modalGeneric").textContent = generic;
+    document.getElementById("modalStrength").textContent = strength;
+    document.getElementById("modalPacking").textContent = packing;
+    document.getElementById("modalManufacturer").textContent = manufacturer;
+
+    document.getElementById("productModal").classList.add("show");
+
+}
+
+document.querySelector(".close-modal").onclick = function(){
+
+    document.getElementById("productModal").classList.remove("show");
+
+}
+
+window.onclick = function(e){
+
+    if(e.target.id==="productModal"){
+
+        document.getElementById("productModal").classList.remove("show");
+
+    }
+};
+// ===============================
+// AUTO OPEN SEARCHED PRODUCT
+// ===============================
+
+const params = new URLSearchParams(window.location.search);
+const searchedProduct = params.get("product");
+
+if (searchedProduct) {
+
+    filter.value = "";
+    manufacturer.value = "";
+    search.value = searchedProduct;
+
+    filterProducts();
+
+    setTimeout(() => {
+
+        const id = searchedProduct
+            .toLowerCase()
+            .replace(/\s+/g, "-");
+
+        const card = document.getElementById(id);
+
+        if (card) {
+
+            // Open accordion
+            const accordion = card.closest(".accordion");
+            accordion.classList.add("active");
+
+            // Scroll to product
+            card.scrollIntoView({
+                behavior: "smooth",
+                block: "center"
+            });
+
+            // Highlight product
+            card.style.border = "3px solid #0077ff";
+            card.style.boxShadow = "0 0 25px rgba(0,119,255,.5)";
+            card.style.background = "#eef7ff";
+
+            setTimeout(() => {
+                card.style.border = "";
+                card.style.boxShadow = "";
+                card.style.background = "";
+            }, 5000);
+
+        }
+
+    }, 300);
+
+}
+// ===============================
+// INITIALIZE PAGE
+// ===============================
+
+displayProducts(products);
+
+search.addEventListener("keyup", filterProducts);
+filter.addEventListener("change", filterProducts);
+manufacturer.addEventListener("change", filterProducts);
