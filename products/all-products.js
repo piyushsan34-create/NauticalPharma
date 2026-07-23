d1df58d3c231b6d921384798e8ab6e4a29005d1a
piyushsan34-create <piyushsan34@gmail.com>
2026-07-23 16:19:41 +0530

    Import Weight Loss products into allProducts array
    Added functionality to import and push Weight Loss products to the allProducts array.

--- products/all-products.js
+++ products/all-products.js
@@ -17,3 +17,19 @@ if (window.edProducts) {
 }
 
 window.allProducts = allProducts;
+
+// Import all Weight Loss products
+if (window.weightProducts) {
+
+    window.weightProducts.forEach(product => {
+
+        allProducts.push({
+            category: "Weight Loss",
+            name: product.name,
+            generic: product.generic,
+            page: `products/weight-loss.html?product=${encodeURIComponent(product.name)}`
+        });
+
+    });
+
+}
