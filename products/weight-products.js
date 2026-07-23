74f0f9762817e7b9ca6ef5fb9650681999a18582
piyushsan34-create <piyushsan34@gmail.com>
2026-07-23 15:30:05 +0530

    Create weight-products.js

---
+++ products/weight-products.js
@@ -0,0 +1,77 @@
+const products = [
+
+{
+    name: "Mounjaro",
+    generic: "Tirzepatide Injection",
+    strength: "2.5mg/0.5ml",
+    packing: "Single Dose Vial",
+    manufacturer: "Eli Lilly"
+},
+
+{
+    name: "Mounjaro",
+    generic: "Tirzepatide Injection",
+    strength: "5mg/0.5ml",
+    packing: "Single Dose Vial",
+    manufacturer: "Eli Lilly"
+},
+
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "2.5mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
+
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "5mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
+
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "7.5mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
+
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "10mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
+
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "12.5mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
+
+{
+    name: "Mounjaro KwikPen",
+    generic: "Tirzepatide Injection",
+    strength: "15mg",
+    packing: "3.0 ml Pre-filled Pen",
+    manufacturer: "Eli Lilly"
+},
+
+{
+    name: "Wegovy FlexTouch",
+    generic: "Semaglutide Injection",
+    strength: "0.25mg",
+    packing: "1.5ml Pre-filled Pen",
+    manufacturer: "Novo Nordisk"
+}
+
+];
+
+window.weightProducts = products;
