339e1a0eec4ef7b1ba3f784ab0e467efcbb7cfb3
piyushsan34-create <piyushsan34@gmail.com>
2026-07-29 18:13:48 +0530

    Rename products variable to hormoneProducts

--- products/hormones-products.js
+++ products/hormones-products.js
@@ -33,7 +33,7 @@ window.hormoneProducts = [
 }
 
 ];
-const products = window.hormoneProducts;
+const hormoneProducts = window.hormoneProducts;
 if (document.getElementById("productContainer")) {
 const search = document.getElementById("searchInput");
 const filter = document.getElementById("categoryFilter");
@@ -162,7 +162,7 @@ function filterhormoneProducts(){
     const generic = filter.value.toLowerCase();
     const company = manufacturer.value.toLowerCase();
 
-    const filtered = products.filter(product => {
+    const filtered = hormoneProducts.filter(product => {
 
         const matchSearch =
             product.name.toLowerCase().includes(keyword) ||
@@ -269,7 +269,7 @@ if (searchedProduct) {
 // INITIALIZE PAGE
 // ===============================
 
-displayhormoneProducts(products);
+displayhormoneProducts(hormoneProducts);
 
 search.addEventListener("keyup", filterhormoneProducts);
 filter.addEventListener("change", filterhormoneProducts);
